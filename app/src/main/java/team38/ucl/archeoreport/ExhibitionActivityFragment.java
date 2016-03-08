c28b1fb79ad068ef4e6e4dc7ee8fdb9e3b8d9de6
Varun Mathur <vkmathur97@yahoo.co.uk>
2016-03-08 23:37:18 +0000

    add spinners and new UI elements

---
+++ app/src/main/java/team38/ucl/archeoreport/ExhibitionActivityFragment.java
@@ -0,0 +1,22 @@
+package team38.ucl.archeoreport;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ExhibitionActivityFragment extends Fragment {
+
+    public ExhibitionActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exhibition, container, false);
+    }
+}
